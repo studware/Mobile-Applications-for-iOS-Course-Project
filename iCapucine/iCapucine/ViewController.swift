4d3eb868531528f6e677b5a426e7407bd6927380
Angela Teneva <studware@Angelas-Mac.local>
2017-04-02 19:41:06 +0300

    Connect the UI to the code

--- iCapucine/iCapucine/ViewController.swift
+++ iCapucine/iCapucine/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var flowerNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    
+    
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        flowerNameLabel.text = "Default text"
     }
-
 
 }
 
