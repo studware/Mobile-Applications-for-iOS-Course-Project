adba5862f8bf0e1dce9c23c7c465cb2e5cd00d5c
Angela Teneva <studware@Angelas-Mac.local>
2017-04-02 12:13:22 +0300

    First commit

---
+++ iCapucine/iCapucine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCapucine
+//
+//  Created by Angela Teneva on 4/2/17.
+//  Copyright © 2017 Telerik Academy - Sofia, Bulgaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
