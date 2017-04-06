53252a53d9f7d04b8f546791e85a59dcc05beae8
Angela Teneva <studware@Angelas-Mac.local>
2017-04-07 00:09:57 +0300

    Created table view-based scene.

---
+++ iCapucine/iCapucine/FlowerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FlowerTableViewCell.swift
+//  iCapucine
+//
+//  Created by Angela Teneva on 4/6/17.
+//  Copyright © 2017 Telerik Academy - Sofia, Bulgaria. All rights reserved.
+//
+
+import UIKit
+
+class FlowerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- iCapucine/iCapucine/RatingControl.swift
+++ iCapucine/iCapucine/RatingControl.swift
@@ -20,7 +20,7 @@ import UIKit
     }
     
     
-    @IBInspectable var starSize: CGSize = CGSize(width: 33.6, height: 33.0){
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
         didSet{
             setupButtons()
         }
