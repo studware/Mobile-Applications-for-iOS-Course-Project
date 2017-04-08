9f97966377a77a0ae9ed40e85a7f0e43d9938a84
Angela Teneva <studware@Angelas-Mac.local>
2017-04-08 20:34:21 +0300

    Implemented interface to display table data.

--- FlowerTableViewController.swift
+++ FlowerTableViewController.swift
@@ -27,24 +27,30 @@ class FlowerTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return flowers.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        // Table view cells are reused and should be dequeued using a cell identifier.
+            let cellIdentifier = "FlowerTableViewCell"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? FlowerTableViewCell  else {
+            fatalError("The dequeued cell is not an instance of FlowerTableViewCell.")
+        }
+        
+            
+        // Fetches the appropriate flower for the data source layout.
+        let flower = flowers[indexPath.row]
+        
+        cell.nameLabel.text = flower.name
+        cell.photoImageView.image = flower.photo
+        cell.ratingControl.rating = flower.rating
+            
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
